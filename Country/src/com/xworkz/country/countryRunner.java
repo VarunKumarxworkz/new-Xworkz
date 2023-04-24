5ea76097b9c208df779c13c9cce1e1373ae3c6b0
VarunKumarxworkz <varunkumarxworkz@gmail.com>
2023-04-24 10:57:15 +0530

    encapsulation

---
+++ Country/src/com/xworkz/country/countryRunner.java
@@ -0,0 +1,75 @@
+package com.xworkz.country;
+
+import com.xworkz.country.data.countryData;
+
+public class countryRunner {
+   
+	public static void main(String[] args) {
+		
+	countryData coun = new countryData();
+	
+	coun.setnoOfstates(28);
+	coun.setnationalfood("RaggiMuddhe");
+	coun.setnationalcolour("Safroon");
+	coun.setnationalFruit("Mango");
+	coun.setnationalAnimal("tiger");
+	coun.setnationalBird("Peacock");
+	coun.setnoOfPeoples(8000000000l);
+	coun.setnationalTree("Ficus bengalensis");
+	coun.setnationalFlower("lotus");
+	coun.setnationalreptial("King cobra");
+	coun.setnationalGame("hockey");
+	coun.setCapital("Delhi");
+	coun.sethighRainfall("cherrapunji");
+	coun.setlowRainfall("NorthEast");
+	coun.setindependence(1947);
+	coun.setRepublic(2601);
+	coun.setnationalAnthem("Jana gana");
+	coun.setAnthemWriter("Rabindranath tagore");
+	coun.setAnthemDuration(52);
+	coun.setnationalCrush("Rashmika Mandana");
+	coun.setfatherOfNation("Gandhi");
+	coun.setFirstpriority("Agriculture");
+	coun.setnationalWonders("MysorePalace");
+	coun.setnationallanguage("Kannada");
+	coun.setnationalPark("bandipur");
+	coun.setnationallSweet("Jalebi");
+	coun.setnationalPrimeMinister("Modi");
+	coun.setnationalPolice("DuraiShingam");
+	coun.setnationalSuperMan("Balayya");
+	coun.setheritageAnimal("Elephant");
+	
+	
+	System.out.println(coun.getnoOfstates());
+	System.out.println(coun.getnationalfood());
+	System.out.println(coun.getnationalFruit());
+	System.out.println(coun.getnationalAnimal());
+	System.out.println(coun.getnationalBird());
+	System.out.println(coun.getnoOfPeoples());
+	System.out.println(coun.getnationalTree());
+	System.out.println(coun.getnationalFlower());
+	System.out.println(coun.getnationalGame());
+	System.out.println(coun.getCapital());
+	System.out.println(coun.gethighRainfall());
+	System.out.println(coun.getlowRainfall());
+	System.out.println(coun.getindependence());
+	System.out.println(coun.getRepublic());
+	System.out.println(coun.getnationalAnthem());
+	System.out.println(coun.getAnthemWriter());
+	System.out.println(coun.getAnthemDuration());
+	System.out.println(coun.getnationalCrush());
+	System.out.println(coun.getfatherOfNation());
+	System.out.println(coun.getFirstpriority());
+	System.out.println(coun.getnationalWonders());
+	System.out.println(coun.getnationallanguage());
+	System.out.println(coun.getnationalPark());
+	System.out.println(coun.getnationalPrimeMinister());
+	System.out.println(coun.getnationalPolice());
+	System.out.println(coun.getnationalSuperMan());
+	System.out.println(coun.getheritageAnimal());
+	System.out.println(coun.getnationallSweet());
+	
+	
+	}	
+	
+}
---
+++ Country/src/com/xworkz/country/data/countryData.java
@@ -0,0 +1,227 @@
+package com.xworkz.country.data;
+
+public class countryData {
+
+	private  int noOFStates;
+	private String nationalfood;
+	private String nationalcolour;
+	private String nationalFruit;
+	private String nationalAnimal;
+	private String nationalBird;
+	private long noOfPeoples;
+	private String nationalTree;
+	private String nationalFlower;
+	private String nationalreptial;
+	private String nationalGame;
+	private String Capital;
+	private String highRainfall;
+	private String lowRainfall;
+	private int independence;
+	private int Republic;
+	private String nationalAnthem;
+	private String AnthemWriter;
+	private int AnthemDuration;
+	private String nationalCrush;
+	private String fatherOfNation;
+	private String Firstpriority;
+	private String nationalWonders;
+	private String nationalPark;
+	private String nationallanguage;
+	private String nationallSweet;
+	private String nationalPrimeMinister;
+	private String nationalPolice;
+	private String nationalSuperMan;
+	private String heritageAnimal;
+	
+	
+	public int getnoOfstates() {
+		return noOFStates;
+	}
+	
+	public String getnationalfood() {
+		return nationalfood;
+	}
+	public String nationalcolour() {
+		return nationalcolour;
+	}
+    public String getnationalFruit() {
+    	return nationalFruit;
+    }
+     public String getnationalAnimal() {
+    	 return nationalAnimal;
+     }
+    public String getnationalBird() {
+    	return nationalBird;
+    }
+    public long getnoOfPeoples() {
+    	return noOfPeoples;
+    }
+    public String getnationalTree() {
+    	return nationalTree;
+    }
+    public String getnationalFlower() {
+    	return nationalFlower;
+    }
+    public String getnationalreptial() {
+    	return nationalreptial;
+    }
+    public String getnationalGame() {
+    	return nationalGame;
+    }
+    public String getCapital() {
+    	return Capital;
+    }
+    public String gethighRainfall() {
+    	return highRainfall;
+    }
+    public String getlowRainfall() {
+    	return lowRainfall;
+    }
+    public int getindependence() {
+    	return independence;
+    }
+    public int getRepublic() {
+    	return Republic;
+    }
+    public String getnationalAnthem() {
+    	return nationalAnthem;
+    }
+    public String getAnthemWriter() {
+    	return AnthemWriter;
+    }
+    public int getAnthemDuration() {
+    	return AnthemDuration;
+    }
+    public String getnationalCrush() {
+    	return nationalCrush;
+    }
+    public String getfatherOfNation() {
+    	return fatherOfNation;
+    }
+    public String getFirstpriority() {
+    	return Firstpriority;
+    }
+    public String getnationalWonders() {
+    	return nationalWonders;
+    }
+    public String getnationalPark() {
+    	return nationalPark;
+    }
+    public String getnationallanguage() {
+    	return nationallanguage;
+    }
+    public String getnationallSweet() {
+    	return nationallSweet;
+    }
+    public String getnationalPrimeMinister() {
+    	return nationalPrimeMinister;
+    }
+    public String getnationalPolice() {
+    	return nationalPolice;
+    }
+    public String getnationalSuperMan() {
+    	return nationalSuperMan;
+    }
+    public String getheritageAnimal() {
+    	return heritageAnimal;
+    }
+    
+    public void  setnoOfstates(int  noOFStates ) {
+    	this. noOFStates =  noOFStates;
+	}
+    public void  setnationalfood(String nationalfood) {
+    	this.nationalfood = nationalfood;
+    }
+    public void setnationalcolour(String nationalcolour) {
+    	this.nationalcolour = nationalcolour;
+    }
+    
+    public void setnationalFruit(String nationalFruit) {
+    	this.nationalFruit = nationalFruit;
+    }
+    public void setnationalAnimal(String nationalAnimal) {
+    	this.nationalAnimal = nationalAnimal;
+    }
+    public void setnationalBird(String nationalBird) {
+    	this.nationalBird = nationalBird;
+    }
+    public void setnoOfPeoples(long noOfPeoples) {
+    	this.noOfPeoples =  noOfPeoples;
+    }
+    public void setnationalTree(String nationalTree) {
+    	this.nationalTree = nationalTree;
+    }
+    public void setnationalFlower(String nationalFlower) {
+    	this.nationalFlower = nationalFlower;
+    }
+    public void setnationalreptial(String nationalreptial) {
+    	this.nationalreptial = nationalreptial;
+    }
+    public void setnationalGame(String nationalGame) {
+    	this.nationalGame = nationalGame;
+    }
+    public void setCapital(String Capital) {
+    	this.Capital = Capital;
+    }
+    public void sethighRainfall(String highRainfall) {
+    	this.highRainfall = highRainfall;
+    }
+    public void setlowRainfall(String lowRainfall) {
+    	this.lowRainfall = lowRainfall;
+    }
+    public void setindependence(int independence) {
+    	this.independence = independence;
+    }
+    public void setRepublic(int Republic) {
+    	this.Republic = Republic;
+    }
+    public void setnationalAnthem(String nationalAnthem) {
+    	this.nationalAnthem = nationalAnthem;
+    }
+    public void setAnthemWriter(String AnthemWriter) {
+    	this.AnthemWriter = AnthemWriter;
+    }
+    public void setAnthemDuration(int AnthemDuration) {
+    	this.AnthemDuration = AnthemDuration;
+    }
+    public void setnationalCrush(String nationalCrush) {
+    	this.nationalCrush = nationalCrush;
+    }
+    public void setfatherOfNation(String fatherOfNation) {
+    	this.fatherOfNation = fatherOfNation;
+    }
+    public void setFirstpriority(String Firstpriority) {
+    	this.Firstpriority = Firstpriority;
+    }
+    public void setnationalWonders(String nationalWonders) {
+    	this.nationalWonders = nationalWonders;
+    }
+	public void setnationalPark(String nationalPark) {
+		this.nationalPark = nationalPark;
+	}
+	public void setnationallanguage(String nationallanguage) {
+		this.nationallanguage = nationallanguage;
+	}
+	public void setnationallSweet(String natioanlSweet) {
+		this.nationallSweet = natioanlSweet;
+	}
+	public void setnationalPrimeMinister(String nationalPrimeMinister) {
+		this.nationalPrimeMinister = nationalPrimeMinister;
+	}
+	public void setnationalPolice(String nationalPolice) {
+		this.nationalPolice = nationalPolice;
+	}
+	public void setnationalSuperMan(String nationalSuperMan) {
+		this.nationalSuperMan = nationalSuperMan;
+	}
+	public void setheritageAnimal(String heritageAnimal) {
+		this.heritageAnimal = heritageAnimal;
+	}
+	
+	
+	
+	
+	
+
+	
+}
