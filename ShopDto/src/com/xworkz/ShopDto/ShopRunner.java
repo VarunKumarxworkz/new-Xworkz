d278136e763f3527d24b613c000a86e8ea4a4fe9
VarunKumarxworkz <varunkumarxworkz@gmail.com>
2023-04-25 10:20:27 +0530

    encapsulation

---
+++ ShopDto/src/com/xworkz/ShopDto/ShopRunner.java
@@ -0,0 +1,145 @@
+package com.xworkz.ShopDto;
+
+import com.xworkz.ShopDto.data.ShopDto;
+
+public class ShopRunner {
+
+	public static void main(String[] args) {
+		
+		ShopDto sop = new ShopDto("varun","BTM","varun",54,"many","Black",46,"denim","gucci","indoor","outdoor","billcounter",6000,"offline","plastic",
+				"welcome",78,"Annai",200,500);
+		
+		sop.setShopName("Varun");
+		sop.setAddress("BTM");
+		sop.setShopOwner("VArun");
+		sop.setnoOfItems(200);
+		sop.setVarity("new");
+		sop.setColour("Black");
+		sop.setSize(46);
+		sop.setDesign("Denim");
+		sop.setBrand("Gucci");
+		sop.setcheckIn("indoor");
+		sop.setcheckOut("outdoor");
+		sop.setCounter("biling");
+		sop.setPrice(6000);
+		sop.setShopingType("Offline");
+		sop.setpackingCover("Plastic");
+		sop.setWishing("Welcome");
+		sop.setnoOfSelections(67);
+		sop.setGroceryStore("Kanna");
+		sop.setitems(200);
+		sop.setAlmondPrice(500);
+		sop.setDatesPrice(200);
+		sop.setpumpkinSeedPrice(200);
+		sop.setChiaSeedsPrice(150);
+		sop.setButterRate(400);
+		sop.setBreadRate(60);
+		sop.setcreatinePrice(1200);
+		sop.setprotienPrice(4000);
+		sop.setJuicePrice(50);
+		sop.setChipsRate(50);
+		sop.seticeCreamPrice(170);
+		sop.setShirtPrice(800);
+		sop.setPantPrice(900);
+		sop.setBeltPrice(700);
+		sop.setshoePrice(2000);
+		sop.setShocksPrice(40);
+		sop.setFruitVarity("vimal");
+		sop.setFruititems(8);
+		sop.setPriceOfApple(250);
+		sop.setPriceOfOrange(80);
+		sop.setPriceOfPomoGranet(200);
+		sop.setWaterMelonPrice(20);
+		sop.setpapayaPrice(40);
+		sop.setMangoPrice(100);
+		sop.setpistaPrice(300);
+		sop.setWallNutPrice(200);
+		sop.setGRBPrice(500);
+		sop.setSweetPrice(250);
+		sop.setBiscuitsPrice(20);
+		sop.setChocolatePrice(30);
+		sop.setCakePrice(50);
+		sop.setMilkPrice(50);
+		sop.setCurdPrice(24);
+		sop.setMilkShakePrice(60);
+		sop.setTvPrice(20000);
+		sop.setTvSize(42);
+		sop.setTvBrand("LG");
+		sop.setMobilePrice(12000);
+		sop.setMobileBrand("VIVO");
+		sop.setheadPhonesPrice(2000);
+		sop.setheadPhonesBrand("SkullCandy");
+		
+		System.out.println(sop.getShopName());
+		System.out.println(sop.getAddress());
+		System.out.println(sop.getShopOwner());
+		System.out.println(sop.getnoOfItems());
+		System.out.println(sop.getVarity());
+		System.out.println(sop.getColour());
+		System.out.println(sop.getSize());
+		System.out.println(sop.getDesign());
+		System.out.println(sop.getBrand());
+		System.out.println(sop.getcheckIn());
+		System.out.println(sop.getcheckOut());
+		System.out.println(sop.getCounter());
+		System.out.println(sop.getPrice());
+		System.out.println(sop.getShopingType());
+		System.out.println(sop.getpackingCover());
+		System.out.println(sop.getWishing());
+		System.out.println(sop.getnoOfSelections());
+		System.out.println(sop.getGroceryStore());
+		System.out.println(sop.getitems());
+		System.out.println(sop.getAlmondPrice());
+		System.out.println(sop.getDatesPrice());
+		System.out.println(sop.getpumpkinSeedPrice());
+		System.out.println(sop.getChiaSeedsPrice());
+		System.out.println(sop.getButterRate());
+		System.out.println(sop.getBreadRate());
+		System.out.println(sop.getcreatinePrice());
+		System.out.println(sop.getprotienPrice());
+		System.out.println(sop.getJuicePrice());
+		System.out.println(sop.getChipsRate());
+		System.out.println(sop.geticeCreamPrice());
+		System.out.println(sop.getShirtPrice());
+		System.out.println(sop.getPantPrice());
+		System.out.println(sop.getBeltPrice());
+		System.out.println(sop.getshoePrice());
+		System.out.println(sop.getShocksPrice());
+		System.out.println(sop.getFruitVarity());
+		System.out.println(sop.getFruititems());
+		System.out.println(sop.getPriceOfApple());
+		System.out.println(sop.getPriceOfOrange());
+		System.out.println(sop.getPriceOfPomoGranet());
+		System.out.println(sop.getWaterMelonPrice());
+		System.out.println(sop.getpapayaPrice());
+		System.out.println(sop.getMangoPrice());
+		System.out.println(sop.getpistaPrice());
+		System.out.println(sop.getWallNutPrice());
+		System.out.println(sop.getGRBPrice());
+		System.out.println(sop.getSweetPrice());
+		System.out.println(sop.getBiscuitsPrice());
+		System.out.println(sop.getChocolatePrice());
+		System.out.println(sop.getCakePrice());
+		System.out.println(sop.getMilkPrice());
+		System.out.println(sop.getCurdPrice());
+		System.out.println(sop.getMilkShakePrice());
+		System.out.println(sop.getTvPrice());
+		System.out.println(sop.getTvSize());
+		System.out.println(sop.getTvBrand());
+		System.out.println(sop.getMobilePrice());
+		System.out.println(sop.getMobileBrand());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
---
+++ ShopDto/src/com/xworkz/ShopDto/data/ShopDto.java
@@ -0,0 +1,464 @@
+package com.xworkz.ShopDto.data;
+
+
+	public class ShopDto {
+
+		private String ShopName;
+		private String Address;
+		private String ShopOwner;
+		private int noOfItems;
+		private String Varity;
+		private String Colour;
+		private int Size;
+		private String Design;
+		private String Brand;
+		private String checkIn;
+		private String checkOut;
+		private String Counter;
+		private int Price;
+		private String ShopingType;
+		private String packingCover;
+		private String Wishing;
+		private int noOfSelections;
+		private String GroceryStore;
+		private int items;
+		private int AlmondPrice;
+		private int DatesPrice;
+		private int pumpkinSeedPrice;
+		private int ChiaSeedsPrice;
+		private int ButterRate;
+		private int BreadRate;
+		private int creatinePrice;
+		private int protienPrice;
+		private int JuicePrice;
+		private int ChipsRate;
+		private int iceCreamPrice;
+		private int ShirtPrice;
+		private int PantPrice;
+		private int BeltPrice;
+		private int shoePrice;
+		private int ShocksPrice;
+		private String FruitVarity;
+		private int Fruititems;
+		private int PriceOfApple;
+		private int PriceOfOrange;
+		private int PriceOfPomoGranet;
+		private int WaterMelonPrice;
+		private int papayaPrice;
+		private int MangoPrice;
+		private int pistaPrice;
+		private int WallNutPrice;
+		private int GRBPrice;
+		private int SweetPrice;
+		private int BiscuitsPrice;
+		private int ChocolatePrice;
+		private int CakePrice;
+		private int MilkPrice;
+		private int CurdPrice;
+		private int MilkShakePrice;
+		private int TvPrice;
+		private int TvSize;
+		private String TvBrand;
+		private int MobilePrice;
+		private String MobileBrand;
+		private int headPhonesPrice;
+		private String headPhonesBrand;
+		
+		
+		public ShopDto(String ShopName, String Address, String ShopOwner, int noOfItems, String Varity, String Colour, int Size, String Design, String Brand, String checkIn, String checkOut, String Counter, int Price, String ShopingType,String packingCover, String Wishing, int noOfSelections, String GroceryStore,int items, int AlmondPrice) {
+			
+		this.ShopName = ShopName;
+		this.Address = Address;
+		this.ShopOwner = ShopOwner;
+		this.noOfItems = noOfItems;
+		this.Varity = Varity;
+		this.Colour = Colour;
+		this.Size = Size;
+		this.Design = Design;
+		this.Brand = Brand;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+		this.Counter = Counter;
+		this.Price = Price;
+		this.ShopingType = ShopingType;
+		this.packingCover = packingCover;
+		this.Wishing = Wishing;
+		this.noOfSelections = noOfSelections;
+		this.GroceryStore = GroceryStore;
+		this.items = items;
+		this.AlmondPrice = AlmondPrice;
+
+	    }
+		
+	    public String getShopName() {
+		return ShopName;
+	    }
+	    public String getAddress() {
+		return Address;
+	    }
+		public String getShopOwner() {
+			return ShopOwner;
+		}
+		public int getnoOfItems() {
+			return noOfItems;
+		}
+		public String getVarity() {
+			return Varity;
+		}
+		public String getColour() {
+			return Colour;
+		}
+		public int  getSize() {
+			return Size;
+		}
+		public String getDesign() {
+			return Design;
+		}
+		public String getBrand() {
+			return Brand;
+		}
+		public String getcheckIn() {
+			return checkIn;
+		}
+		public String getcheckOut() {
+			return checkOut;
+		}
+		public String getCounter() {
+			return Counter;
+		}
+		public int getPrice() {
+			return Price;
+		}
+		public String getShopingType() {
+			return ShopingType;
+		}
+		public String getpackingCover() {
+			return packingCover;
+		}
+		public String getWishing() {
+			return Wishing;
+		}
+		public int getnoOfSelections() {
+			return noOfSelections;
+		}
+		public String getGroceryStore() {
+			return GroceryStore;
+		}
+		public int getitems() {
+			return items;
+		}
+		public int getAlmondPrice() {
+			return AlmondPrice;
+		}
+		public int getDatesPrice() {
+			return DatesPrice;
+		}
+		public int getpumpkinSeedPrice() {
+			return pumpkinSeedPrice;
+		}
+		public int getChiaSeedsPrice() {
+			return ChiaSeedsPrice;
+		}
+		public int getButterRate() {
+			return ButterRate;
+		}
+		public int getBreadRate() {
+			return BreadRate;
+		}
+		public int getcreatinePrice() {
+			return creatinePrice;
+		}
+		public int getprotienPrice() {
+			return protienPrice;
+		}
+		public int getJuicePrice() {
+			return JuicePrice;
+		}
+		public int getChipsRate() {
+			return ChipsRate;
+		}
+		public int geticeCreamPrice() {
+			return iceCreamPrice;
+		}
+		public int getShirtPrice() {
+			return ShirtPrice;
+		}
+		public int getPantPrice() {
+			return PantPrice;
+		}
+		public int getBeltPrice() {
+			return BeltPrice;
+		}
+		public int getshoePrice() {
+			return shoePrice;
+		}
+		public int getShocksPrice() {
+			return ShocksPrice;
+		}
+		public String getFruitVarity() {
+			return FruitVarity;
+		}
+		public int getFruititems() {
+			return Fruititems;
+		}
+		public int getPriceOfApple() {
+			return PriceOfApple;
+		}
+		public int getPriceOfOrange() {
+			return PriceOfOrange;
+		}
+		public int getPriceOfPomoGranet() {
+			return PriceOfPomoGranet;
+		}
+		public int getWaterMelonPrice() {
+			return WaterMelonPrice;
+		}
+		public int getpapayaPrice() {
+			return papayaPrice;
+		}
+		public int getMangoPrice() {
+			return MangoPrice;
+		}
+		public int getpistaPrice() {
+			return pistaPrice;
+		}
+		public int getWallNutPrice() {
+			return WallNutPrice;
+		}
+		public int getGRBPrice() {
+			return GRBPrice;
+		}
+		public int getSweetPrice() {
+			return SweetPrice;
+		}
+		public int getBiscuitsPrice() {
+			return BiscuitsPrice;
+		}
+		public int getChocolatePrice() {
+			return ChocolatePrice;
+		}
+		public int getCakePrice() {
+			return CakePrice;
+		}
+		public int getMilkPrice() {
+			return MilkPrice;
+		}
+		public int getCurdPrice() {
+			return CurdPrice;
+		}
+		public int getMilkShakePrice() {
+			return MilkShakePrice;
+		}
+		public int getTvPrice() {
+			return TvPrice;
+		}
+		public int getTvSize() {
+			return TvSize;
+		}
+		public String getTvBrand() {
+			return TvBrand;
+		}
+		public int getMobilePrice() {
+			return MobilePrice;
+		}
+		public String getMobileBrand() {
+			return MobileBrand;
+		}
+		public int headPhonesPrice() {
+			return headPhonesPrice;
+		}
+		public String headPhonesBrand() {
+			return headPhonesBrand;
+		}
+		
+		
+		
+		
+		
+		
+		public void setShopName(String ShopName) {
+			this.ShopName = ShopName;
+		}
+		public void setAddress(String Address) {
+			this.Address = Address;
+		}
+		public void setShopOwner(String ShopOwner) {
+			this.ShopOwner = ShopOwner;
+		}
+		public void setnoOfItems(int noOfItems) {
+			this.noOfItems = noOfItems;
+		}
+		public void setVarity(String Varity) {
+			this.Varity = Varity;
+		}
+		public void setColour(String Colour) {
+			this.Colour = Colour;
+		}
+		public void setSize(int Size) {
+			this.Size = Size;
+		}
+		public void setDesign(String Design) {
+			this.Design = Design;
+		}
+		public void setBrand(String Brand) {
+			this.Brand = Brand;
+		}
+		public void setcheckIn(String checkIn) {
+			this.checkIn = checkIn;
+		}
+		public void setcheckOut(String checkOut) {
+			this.checkOut = checkOut;
+		}
+		public void setCounter(String Counter) {
+			this.Counter = Counter;
+		}
+		public void setPrice(int Price) {
+			this.Price = Price;
+		}
+		public void setShopingType(String ShopingType) {
+			this.ShopingType = ShopingType;
+		}
+		public void setpackingCover(String packingCover) {
+			this.packingCover = packingCover;
+		}
+		public void setWishing(String Wishing) {
+			this.Wishing = Wishing;
+		}
+		public void setnoOfSelections(int noOfSelections) {
+			this.noOfSelections = noOfSelections;
+		}
+		public void setGroceryStore(String GroceryStore) {
+			this.GroceryStore = GroceryStore;
+		}
+		public void setitems(int items) {
+			this.items = items;
+		}
+		public void setAlmondPrice(int AlmondPrice) {
+			this.AlmondPrice = AlmondPrice;
+		}
+		public void setDatesPrice(int DatesPrice) {
+			this.DatesPrice = DatesPrice;
+		}
+		public void setpumpkinSeedPrice(int pumpkinSeedPrice) {
+			this.pumpkinSeedPrice = pumpkinSeedPrice;
+		}
+		public void setChiaSeedsPrice(int ChiaSeedsPrice) {
+			this.ChiaSeedsPrice = ChiaSeedsPrice;
+		}
+		public void setButterRate(int ButterRate) {
+			this.ButterRate = ButterRate;
+		}
+		public void setBreadRate(int BreadRate) {
+			this.BreadRate =  BreadRate;
+			
+		}
+		public void setcreatinePrice(int creatinePrice) {
+			this.creatinePrice = creatinePrice;
+		}
+		public void setprotienPrice(int protienPrice) {
+			this.protienPrice = protienPrice;
+		}
+		public void setJuicePrice(int JuicePrice) {
+			this.JuicePrice = JuicePrice;
+		}
+		public void setChipsRate(int ChipsRate) {
+			this.ChipsRate = ChipsRate;
+		}
+		public void seticeCreamPrice(int iceCreamPrice) {
+			this.iceCreamPrice = iceCreamPrice;
+		}
+		public void setShirtPrice(int ShirtPrice) {
+			this.ShirtPrice = ShirtPrice;
+		}
+		public void setPantPrice(int PantPrice) {
+			this.PantPrice = PantPrice;
+		}
+		public void setBeltPrice(int BeltPrice) {
+			this.BeltPrice = BeltPrice;
+		}
+		public void setshoePrice(int shoePrice) {
+			this.shoePrice = shoePrice;
+		}
+		public void setShocksPrice(int ShocksPrice) {
+			this.ShocksPrice = ShocksPrice;
+		}
+		public void setFruitVarity(String FruitVarity) {
+			this.FruitVarity = FruitVarity;
+		}
+		public void setFruititems(int Fruititems) {
+			this.Fruititems = Fruititems;
+		}
+		public void setPriceOfApple(int PriceOfApple) {
+			this.PriceOfApple = PriceOfApple;
+		}
+		public void setPriceOfOrange(int PriceOfOrange) {
+			this.PriceOfOrange = PriceOfOrange;
+		}
+		public void setPriceOfPomoGranet(int PriceOfPomoGranet) {
+			this.PriceOfPomoGranet = PriceOfPomoGranet;
+		}
+		public void setWaterMelonPrice(int WaterMelonPrice) {
+			this.WaterMelonPrice = WaterMelonPrice;
+		}
+		public void setpapayaPrice(int papayaPrice) {
+			this.papayaPrice = papayaPrice;
+		}
+		public void setMangoPrice(int MangoPrice) {
+			this.MangoPrice = MangoPrice;
+		}
+		public void setpistaPrice(int pistaPrice) {
+			this.pistaPrice = pistaPrice;
+		}
+		public void setWallNutPrice(int WallNutPrice) {
+			this.WallNutPrice = WallNutPrice;
+		}
+		public void setGRBPrice(int GRBPrice) {
+			this.GRBPrice = GRBPrice;
+		}
+		public void setSweetPrice(int SweetPrice) {
+			this.SweetPrice = SweetPrice;
+		}
+		public void setBiscuitsPrice(int BiscuitsPrice) {
+			this.BiscuitsPrice = BiscuitsPrice;
+		}
+		public void setChocolatePrice(int ChocolatePrice) {
+			this.ChocolatePrice = ChocolatePrice;
+		}
+		public void setCakePrice(int CakePrice) {
+			this.CakePrice = CakePrice;
+		}
+		public void setMilkPrice(int MilkPrice) {
+		     this.MilkPrice = MilkPrice;	
+		}
+		public void setCurdPrice(int CurdPrice) {
+			this.CurdPrice = CurdPrice;
+		}
+		public void setMilkShakePrice(int MilkShakePrice) {
+			this.MilkShakePrice = MilkShakePrice;
+		}
+		public void setTvPrice(int TvPrice) {
+			this.TvPrice = TvPrice;
+		}
+		public void setTvSize(int TvSize) {
+			this.TvSize = TvSize;
+		}
+		public void setTvBrand(String TvBrand) {
+			this.TvBrand = TvBrand;
+		}
+		public void setMobilePrice(int MobilePrice) {
+			this.MobilePrice = MobilePrice;
+		}
+	    public void setMobileBrand(String MobileBrand) {
+	    	this.MobileBrand = MobileBrand;
+	    }
+		public void setheadPhonesPrice(int headPhonesPrice) {
+			this.headPhonesPrice = headPhonesPrice;
+		}
+		public void setheadPhonesBrand(String headPhonesBrand) {
+			this.headPhonesBrand = headPhonesBrand;
+		}
+		
+		
+		
+	
+	}
